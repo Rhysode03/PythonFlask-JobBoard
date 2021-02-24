b02e94cc5e15d7d843230fa4d1f24c2e44254537
Kyle Calder <kylebcalder03@gmail.com>
2021-02-24 13:05:40 -0700

    fixed typo in app.py

--- jobs/app.py
+++ jobs/app.py
@@ -25,9 +25,9 @@ def execute_sql(sql, values=(), commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connction = getattr(g, '_connection', None)
-    if connction is not None:
-        connction.close()
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
         
 @app.route('/')
 @app.route('/jobs')
